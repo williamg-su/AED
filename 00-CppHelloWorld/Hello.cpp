7a1adea6f4206e13b3cdb46abf68ea64e54d82e6
William Maximiliano Garay <wgaray@frba.utn.edu.ar>
2023-04-27 15:07:09 -0300

    Delete 00-CppHelloWorld directory

--- 00-CppHelloWorld/Hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout /* cout stream de salida */<< "Hello World!";
-    return 0; /*0 por convencion significa que no hubo error, si no se pone el programa por defecto retorna 0  */
-}
--- 00-CppHelloWorld/readme.md
+++
@@ -1,2 +0,0 @@
-Compilador elegido: G++ 12
-con C++ version 20
